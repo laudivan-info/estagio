84990a7c650d3cca232095e57aecfcc016f5cce6
Laudivan Freire de Almeida <laudivan@gmail.com>
2020-01-09 10:41:54 -0300

    Ajustes de código e estilo

---
+++ projeto/cadlicitacao/hdocs/javascript/function.js
@@ -0,0 +1,31 @@
+
+/**
+ * Change to page with id passed by clickToPage attribute
+ * 
+ * @param {EventTarget} event 
+ */
+function clickToPage(event) {
+    target = event.target
+
+    toPageId = $(target).attr('clickToPage')
+
+    if (!toPageId) return
+
+    changePageById(toPageId)
+}
+
+/**
+ * 
+ * @param {String} toPageId 
+ */
+function changePageById(toPageId) {
+    toPage = $('#' + toPageId)
+
+    $('section:not(hidden)').addClass('hidden').fadeOut('fast')
+
+    $(toPage).removeClass('hidden').fadeIn('slow')
+}
+
+
+
+/* Funções de acesso ao FireBase */
